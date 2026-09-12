73dc0f10e6cfee2a47b69705f45ddcee6482aa10
Nala <24021544@vnu.edu.vn>
2026-09-13 01:27:22 +0700

    bai 3

--- api_session_01/app.py
+++ api_session_01/app.py
@@ -9,16 +9,37 @@
 
 
 # Bai 2
+# from flask import Flask,jsonify,request
+# app = Flask(__name__)
+# # GET /health — kiểm tra server còn sống
+# @app.route("/health", methods=["GET"])
+# def health():
+#     return jsonify({"status": "Circle K"}), 200
+# # POST /chocolate — trả lại cái client gửi
+# @app.route("/chocolate", methods=["POST"])
+# def chocolate():
+#     data = request.get_json(silent=True) or {}
+#     return jsonify({"you_sent": data}), 200
+# if __name__ == "__main__":
+#     app.run(host="127.0.0.1", port=5000, debug=True)
+
+# bai 3
 from flask import Flask,jsonify,request
+from uuid import uuid4
 app = Flask(__name__)
-# GET /health — kiểm tra server còn sống
-@app.route("/health", methods=["GET"])
-def health():
-    return jsonify({"status": "Circle K"}), 200
-# POST /chocolate — trả lại cái client gửi
-@app.route("/chocolate", methods=["POST"])
-def chocolate():
-    data = request.get_json(silent=True) or {}
-    return jsonify({"you_sent": data}), 200
+STUDENTS = []
+@app.route("/students", methods= ["POST"])
+def create_students():
+    body = request .get_json(silent=True) or {}
+    name = body.get("name")
+    if not name:
+        return jsonify({"error":"NEED A NAME"}), 400
+    student = {
+        "id": str(uuid4()),
+        "name": name,
+        "gpa": body.get("gpa", 0.0),
+    }
+    STUDENTS.append(student)
+    return jsonify(student), 201
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=5000, debug=True)
